0e947f08d75bb3229ac61086dcd22f9a3be14ae3
BharathVishal <bharathvishalg@gmail.com>
2019-12-07 14:07:30 +0530

    Initial commit 2
    Initial commit 2

--- app/src/main/java/com/bharathvishal/messagecommunicationusingwearabledatalayer/MainActivity.kt
+++ app/src/main/java/com/bharathvishal/messagecommunicationusingwearabledatalayer/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.bharathvishal.messagecommunicationusingwearabledatalayer
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.wearable.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), DataClient.OnDataChangedListener, MessageClient.OnMessageReceivedListener,
+CapabilityClient.OnCapabilityChangedListener{
+    var activityContext: Context? =null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        activityContext=this
+    }
+
+    override fun onDataChanged(p0: DataEventBuffer) {
+    }
+
+    override fun onMessageReceived(p0: MessageEvent) {
+    }
+
+    override fun onCapabilityChanged(p0: CapabilityInfo) {
     }
 }
--- wear/src/main/java/com/bharathvishal/messagecommunicationusingwearabledatalayer/MainActivity.kt
+++ wear/src/main/java/com/bharathvishal/messagecommunicationusingwearabledatalayer/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.bharathvishal.messagecommunicationusingwearabledatalayer
 
+import android.content.Context
 import android.os.Bundle
 import android.support.wearable.activity.WearableActivity
+import com.google.android.gms.wearable.*
 
-class MainActivity : WearableActivity() {
+class MainActivity : WearableActivity(), DataClient.OnDataChangedListener,
+    MessageClient.OnMessageReceivedListener,
+    CapabilityClient.OnCapabilityChangedListener {
+    var activityContext: Context? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -11,5 +16,17 @@ class MainActivity : WearableActivity() {
 
         // Enables Always-on
         setAmbientEnabled()
+        activityContext = this
+
+
+    }
+
+    override fun onDataChanged(p0: DataEventBuffer) {
+    }
+
+    override fun onMessageReceived(p0: MessageEvent) {
+    }
+
+    override fun onCapabilityChanged(p0: CapabilityInfo) {
     }
 }
