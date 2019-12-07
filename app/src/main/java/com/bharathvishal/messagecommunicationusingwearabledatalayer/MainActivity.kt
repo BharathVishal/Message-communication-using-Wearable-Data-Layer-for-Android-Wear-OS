ea9ec62b997334429d32c8b909db7ef50c1598b4
BharathVishal <bharathvishalg@gmail.com>
2019-12-07 14:43:36 +0530

    commit 3
    commit 3

--- app/src/main/java/com/bharathvishal/messagecommunicationusingwearabledatalayer/MainActivity.kt
+++ app/src/main/java/com/bharathvishal/messagecommunicationusingwearabledatalayer/MainActivity.kt
@@ -1,20 +1,204 @@
 package com.bharathvishal.messagecommunicationusingwearabledatalayer
 
+import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
+import android.os.AsyncTask
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.tasks.Tasks
 import com.google.android.gms.wearable.*
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
-class MainActivity : AppCompatActivity(), DataClient.OnDataChangedListener, MessageClient.OnMessageReceivedListener,
-CapabilityClient.OnCapabilityChangedListener{
-    var activityContext: Context? =null
+class MainActivity : AppCompatActivity(), DataClient.OnDataChangedListener,
+    MessageClient.OnMessageReceivedListener,
+    CapabilityClient.OnCapabilityChangedListener {
+    var activityContext: Context? = null
+    private val wearableAppCheckPayload = "AppOpenWearable"
+    private val wearableAppCheckPayloadReturnACK = "AppOpenWearableACK"
+    private var wearableDeviceConnected: Boolean = false
 
+
+    //This string holds the file names of the received images on the wearable device
+    private val currentlyReceievedMessageFromWear: String? = null
+    //This string holds the acknowledgement payload response sent from wear
+    private val currentAckFromWearForAppOpenCheck: String? = null
+    private val APP_OPEN_WEARABLE_PAYLOAD_PATH = "/APP_OPEN_WEARABLE_PAYLOAD"
+
+    private val TAG_GET_NODES: String = "getnodes1"
+
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        activityContext=this
+        activityContext = this
+        wearableDeviceConnected = false
+
+        checkwearablesButton.setOnClickListener {
+            val tempAct: Activity = activityContext as MainActivity
+            AsyncTask.execute {
+                try {
+                    val getNodesResBool =
+                        getNodes(tempAct.applicationContext)
+
+                    if (getNodesResBool!![0]) {
+                        //if message Acknowlegement Received
+                        if (getNodesResBool[1]) {
+                            Toast.makeText(
+                                activityContext,
+                                "Wearable device paired and app is open. Tap the \"Send Message to Wearable\" button to send the message to your wearable device.",
+                                Toast.LENGTH_LONG
+                            ).show()
+                            deviceconnectionStatusTv.text =
+                                "Wearable device paired and app is open."
+                            deviceconnectionStatusTv.visibility = View.VISIBLE
+                            wearableDeviceConnected = true
+                            sendmessageButton.visibility = View.VISIBLE
+                        } else {
+                            Toast.makeText(
+                                activityContext,
+                                "A wearable device is paired but the wearable app on your watch isn't open. Launch the wearable app and try again.",
+                                Toast.LENGTH_LONG
+                            ).show()
+                            deviceconnectionStatusTv.text =
+                                "Wearable device paired but app isn't open."
+                            deviceconnectionStatusTv.visibility = View.VISIBLE
+                            wearableDeviceConnected = false
+                            sendmessageButton.visibility = View.GONE
+                        }
+                    } else {
+                        Toast.makeText(
+                            activityContext,
+                            "No wearable device paired. Pair a wearable device to your phone using the Wear OS app and try again.",
+                            Toast.LENGTH_LONG
+                        ).show()
+                        deviceconnectionStatusTv.text = "Wearable device not paired and connected."
+                        deviceconnectionStatusTv.visibility = View.VISIBLE
+                        wearableDeviceConnected = false
+                        sendmessageButton.visibility = View.GONE
+                    }
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+        }
+
+
+
+        sendmessageButton.setOnClickListener {
+            if (wearableDeviceConnected) {
+                //Send message to the wearable device
+            }
+        }
+
     }
+
+
+    private fun getNodes(context: Context): BooleanArray? {
+        val nodeResults = HashSet<String>()
+        val resBool = BooleanArray(2)
+        resBool[0] = false //nodePresent
+        resBool[1] = false //wearableReturnAckReceived
+        val nodeListTask =
+            Wearable.getNodeClient(context).connectedNodes
+        try {
+            // Block on a task and get the result synchronously (because this is on a background thread).
+            val nodes =
+                Tasks.await(
+                    nodeListTask
+                )
+            Log.e(TAG_GET_NODES, "Task fetched nodes")
+            for (node in nodes) {
+                Log.e(TAG_GET_NODES, "inside loop")
+                nodeResults.add(node.id)
+                try {
+                    val nodeId = node.id
+                    // Set the data of the message to be the bytes of the Uri.
+                    val payload: ByteArray = wearableAppCheckPayload.toByteArray()
+                    // Send the rpc
+                    // Instantiates clients without member variables, as clients are inexpensive to
+                    // create. (They are cached and shared between GoogleApi instances.)
+                    val sendMessageTask =
+                        Wearable.getMessageClient(context)
+                            .sendMessage(nodeId, APP_OPEN_WEARABLE_PAYLOAD_PATH, payload)
+                    try {
+                        // Block on a task and get the result synchronously (because this is on a background thread).
+                        val result = Tasks.await(sendMessageTask)
+                        Log.d(TAG_GET_NODES, "send message result : $result")
+                        resBool[0] = true
+                        //Wait for 1000 ms/1 sec for the acknowledgement message
+                        //Quantum 1
+                        if (currentAckFromWearForAppOpenCheck != wearableAppCheckPayloadReturnACK) {
+                            Thread.sleep(100)
+                            Log.d(TAG_GET_NODES, "ACK thread sleep quantum 1")
+                        }
+                        if (currentAckFromWearForAppOpenCheck == wearableAppCheckPayloadReturnACK) {
+                            resBool[1] = true
+                            return resBool
+                        }
+                        //Quantum 2
+                        if (currentAckFromWearForAppOpenCheck != wearableAppCheckPayloadReturnACK) {
+                            Thread.sleep(150)
+                            Log.d(TAG_GET_NODES, "ACK thread sleep quantum 2")
+                        }
+                        if (currentAckFromWearForAppOpenCheck == wearableAppCheckPayloadReturnACK) {
+                            resBool[1] = true
+                            return resBool
+                        }
+                        //Quantum 3
+                        if (currentAckFromWearForAppOpenCheck != wearableAppCheckPayloadReturnACK) {
+                            Thread.sleep(200)
+                            Log.d(TAG_GET_NODES, "ACK thread sleep quantum 3")
+                        }
+                        if (currentAckFromWearForAppOpenCheck == wearableAppCheckPayloadReturnACK) {
+                            resBool[1] = true
+                            return resBool
+                        }
+                        //Quantum 4
+                        if (currentAckFromWearForAppOpenCheck != wearableAppCheckPayloadReturnACK) {
+                            Thread.sleep(250)
+                            Log.d(TAG_GET_NODES, "ACK thread sleep quantum 4")
+                        }
+                        if (currentAckFromWearForAppOpenCheck == wearableAppCheckPayloadReturnACK) {
+                            resBool[1] = true
+                            return resBool
+                        }
+                        //Quantum 5
+                        if (currentAckFromWearForAppOpenCheck != wearableAppCheckPayloadReturnACK) {
+                            Thread.sleep(350)
+                            Log.d(TAG_GET_NODES, "ACK thread sleep quantum 5")
+                        }
+                        if (currentAckFromWearForAppOpenCheck == wearableAppCheckPayloadReturnACK) {
+                            resBool[1] = true
+                            return resBool
+                        }
+                        resBool[1] = false
+                        Log.d(
+                            TAG_GET_NODES,
+                            "ACK thread timeout, no message received from the wearable "
+                        )
+                    } catch (exception: Exception) {
+                        Log.d(TAG_GET_NODES, "send message exception")
+                        exception.printStackTrace()
+                    }
+                } catch (e1: Exception) {
+                    Log.d(TAG_GET_NODES, "send message exception")
+                    e1.printStackTrace()
+                }
+            } //end of for loop
+        } catch (exception: Exception) {
+            Log.e(TAG_GET_NODES, "Task failed: $exception")
+            exception.printStackTrace()
+        }
+        return resBool
+    }
+
 
     override fun onDataChanged(p0: DataEventBuffer) {
     }
@@ -23,5 +207,30 @@ CapabilityClient.OnCapabilityChangedListener{
     }
 
     override fun onCapabilityChanged(p0: CapabilityInfo) {
+    }
+
+
+    override fun onPause() {
+        super.onPause()
+        try {
+            Wearable.getDataClient(activityContext!!).removeListener(this)
+            Wearable.getMessageClient(activityContext!!).removeListener(this)
+            Wearable.getCapabilityClient(activityContext!!).removeListener(this)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+
+    override fun onResume() {
+        super.onResume()
+        try {
+            Wearable.getDataClient(activityContext!!).addListener(this)
+            Wearable.getMessageClient(activityContext!!).addListener(this)
+            Wearable.getCapabilityClient(activityContext!!)
+                .addListener(this, Uri.parse("wear://"), CapabilityClient.FILTER_REACHABLE)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 }
--- wear/src/main/java/com/bharathvishal/messagecommunicationusingwearabledatalayer/MainActivity.kt
+++ wear/src/main/java/com/bharathvishal/messagecommunicationusingwearabledatalayer/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.bharathvishal.messagecommunicationusingwearabledatalayer
 
 import android.content.Context
+import android.net.Uri
 import android.os.Bundle
 import android.support.wearable.activity.WearableActivity
 import com.google.android.gms.wearable.*
@@ -28,5 +29,30 @@ class MainActivity : WearableActivity(), DataClient.OnDataChangedListener,
     }
 
     override fun onCapabilityChanged(p0: CapabilityInfo) {
+    }
+
+
+    override fun onPause() {
+        super.onPause()
+        try {
+            Wearable.getDataClient(activityContext!!).removeListener(this)
+            Wearable.getMessageClient(activityContext!!).removeListener(this)
+            Wearable.getCapabilityClient(activityContext!!).removeListener(this)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
+
+    override fun onResume() {
+        super.onResume()
+        try {
+            Wearable.getDataClient(activityContext!!).addListener(this)
+            Wearable.getMessageClient(activityContext!!).addListener(this)
+            Wearable.getCapabilityClient(activityContext!!)
+                .addListener(this, Uri.parse("wear://"), CapabilityClient.FILTER_REACHABLE)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 }
