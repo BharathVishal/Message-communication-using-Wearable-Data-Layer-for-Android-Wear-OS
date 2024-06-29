8f9ced0527d180409893049c9edb8362c1d7f89a
Bharath Vishal G <bharathvishalg@gmail.com>
2024-06-29 14:27:38 +0530

    Development commit
    - Upgraded gradle plugin to 8.5.0.
    - Migrated build configuration files from Groovy to Kotlin.
    - Migrated build to version catalogs (libs.versions.toml).
    - Updated all internal dependencies.

---
+++ settings.gradle.kts
@@ -0,0 +1,26 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven {
+            setUrl("https://jitpack.io")
+        }
+    }
+}
+
+rootProject.name = "Message communication using Wearable Data Layer"
+include(":app",":wear")
