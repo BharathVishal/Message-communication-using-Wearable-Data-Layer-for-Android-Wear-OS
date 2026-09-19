6f0155f9c1bc9bbbda00d4888c47a1dbc6e48614
Bharath Vishal G <bharathvishalg@gmail.com>
2026-09-19 19:11:11 +0530

    dev commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,16 +9,16 @@ plugins {
 extensions.configure<ApplicationExtension> {
     buildFeatures.buildConfig = false
     namespace = "com.bharathvishal.messagecommunicationusingwearabledatalayer"
-    compileSdk = 36
+    compileSdk = 37
     defaultConfig {
         applicationId = "com.bharathvishal.messagecommunicationusingwearabledatalayer"
         vectorDrawables {
             useSupportLibrary = true
         }
         minSdk = 23
-        targetSdk = 36
-        versionCode = 128
-        versionName = "4.8"
+        targetSdk = 37
+        versionCode = 129
+        versionName = "4.9"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     buildFeatures {
@@ -75,7 +75,6 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
     implementation(libs.androidx.cardview)
     implementation(libs.androidx.compose.material3)
-    implementation(libs.androidx.compose.material3.windowsize)
     implementation(libs.androidx.compose.material3.windowsize)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
